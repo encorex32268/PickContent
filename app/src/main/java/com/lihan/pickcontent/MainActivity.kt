0ab1e29ba73ecc44a90183aa14a398892cffb450
LiHan <encorex32268@gmail.com>
2022-12-18 14:35:11 +0900

    Add get file name

--- app/src/main/java/com/lihan/pickcontent/MainActivity.kt
+++ app/src/main/java/com/lihan/pickcontent/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.lihan.pickcontent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.util.Log
 import android.webkit.MimeTypeMap
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import com.lihan.pickcontent.databinding.ActivityMainBinding
 import java.io.File
 
@@ -20,13 +24,44 @@ class MainActivity : AppCompatActivity() {
                 val contentResolver = this@MainActivity.contentResolver
                 val mineType = MimeTypeMap.getSingleton()
                 val fileType = mineType.getExtensionFromMimeType(contentResolver.getType(uri))
-                val file = File(cacheDir , "file")
-                file.createNewFile()
-                file.outputStream().use {
-                  this@MainActivity.contentResolver.openInputStream(uri)?.copyTo(it)
+
+                Log.d("TAG", " :${uri.scheme} ")
+                if (uri.scheme?.contains("content") == true) {
+                    var fileName : String? = null
+                    val cursor = contentResolver.query(
+                        uri,null,null,null,null
+                    )
+                    try {
+                        if (cursor!=null && cursor.moveToFirst()){
+                            fileName = cursor.getString(
+                                 cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                            )
+                            Log.d("TAG", " cursor!=null  :  ${fileName}")
+                        }
+                    }catch (e : Exception){
+                        e.stackTrace
+                    }finally {
+                        cursor?.close()
+                    }
+                    if (fileName == null){
+                        fileName = uri.path
+                        val cutt : Int = fileName?.lastIndexOf('/')?:0
+                        if (cutt != -1){
+                            fileName = fileName?.substring((cutt + 1))
+                        }
+                    }
+                    Log.d("TAG", " fileName >>> ${fileName}")
                 }
-                binding.fileNameTextView.text = file.path
-                Log.d("TAG", "Yes Can Send  this file : $fileType")
+
+
+
+//                val file = File(cacheDir , "file")
+//                file.createNewFile()
+//                file.outputStream().use {
+//                    contentResolver.openInputStream(uri)?.copyTo(it)
+//                }
+//                binding.fileNameTextView.text = file.path
+//                Log.d("TAG", "Yes Can Send  this file : $fileType")
 
         }
     }
@@ -41,6 +76,7 @@ class MainActivity : AppCompatActivity() {
                 requestFilePickerLauncher.launch(
                     "*/*"
                 )
+
             }
         }
     }
